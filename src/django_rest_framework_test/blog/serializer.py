fcc2fdf9ec2406441dc3167f0d40975d871a79be
Kimihiro n <kimihiro_n@pistatium.dip.jp>
2015-12-12 21:08:55 +0900

    Expand author model

--- src/django_rest_framework_test/blog/serializer.py
+++ src/django_rest_framework_test/blog/serializer.py
@@ -8,11 +8,13 @@ from .models import User, Entry
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'mail')
+        fields = ('id', 'name', 'mail')
 
 
 class EntrySerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
     class Meta:
         model = Entry
-        fields = ('title', 'body', 'created_at', 'status', 'author')
+        fields = ('id', 'title', 'body', 'created_at', 'status', 'author')
 
