17a68c45de19c14fc1d540fc7fc284a12875ffb1
Kimihiro n <kimihiro_n@pistatium.dip.jp>
2015-12-12 21:39:26 +0900

    Add pager

--- src/django_rest_framework_test/django_rest_framework_test/settings.py
+++ src/django_rest_framework_test/django_rest_framework_test/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
+}
